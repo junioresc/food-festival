50fa5b251c029855c7544d06400f35dc3bbdb92d
Junioresc <junioresc1092@gmail.com>
2020-10-06 16:32:12 -0400

    added file loader and image-webpack-loader to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require(`path`);
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require(`webpack-bundle-analyzer`).BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: `./assets/js/script.js`,
+    entry: {
+        app: `./assets/js/script.js`,
+        events: `./assets/js/events.js`,
+        schedule: `./assets/js/schedule.js`,
+        tickets: `./assets/js/tickets.js`
+    },
     output: {
         path: path.resolve(__dirname, `dist`),
-        filename: `main.bundle.js`
+        filename: `[name].bundle.js`
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: `file-loader`,
+                        options: {
+                            name (file) {
+                                return `[path][name].[ext]`
+                            },
+                            publicPath: function(url) {
+                                return url.replace(`../`, `/assets/`)
+                            }
+                        }
+                    },
+                    {
+                        loader: `image-webpack-loader`
+                    }
+                ]
+            }
+        ],
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: `jquery`,
             jQuery: `jquery`
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: `static`,
+        })
     ],
     mode: `development`
 };
